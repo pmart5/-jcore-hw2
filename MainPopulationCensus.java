f2047b1615ecda24ce581584af2bc85d497b60ed
Пётр Мартынчик <pmart5a@yandex.ru>
2022-02-24 19:45:56 +0400

    Измение численности населения в MainPopulationCensus.java

--- MainPopulationCensus.java
+++ MainPopulationCensus.java
@@ -10,7 +10,7 @@ public class MainPopulationCensus {
         List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
         List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
         Collection<Person> persons = new ArrayList<>();
-        for (int i = 0; i < 100_000; i++) {
+        for (int i = 0; i < 10_000_000; i++) {
             persons.add(new Person(
                     names.get(new Random().nextInt(names.size())),
                     families.get(new Random().nextInt(families.size())),
